d7b853b6f915d2414acb800e80e3d4c23d2b2586
neverland <jait.chen@foxmail.com>
2026-01-06 15:18:49 +0800

    fix: update Rsbuild e2e scripts (#8)

--- tests/rspack/rsbuild.ts
+++ tests/rspack/rsbuild.ts
@@ -1,16 +1,11 @@
 import type { RunOptions } from '../../types';
-import { $, cd, runInRepo } from '../../utils';
+import { runInRepo } from '../../utils';
 
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
     repo: 'web-infra-dev/rsbuild',
     branch: process.env.RSBUILD_REF ?? 'main',
-    beforeTest: async () => {
-      cd('./e2e');
-      await $`pnpm playwright install chromium`;
-      cd('..');
-    },
-    test: ['e2e:rspack'],
+    test: ['e2e'],
   });
 }
