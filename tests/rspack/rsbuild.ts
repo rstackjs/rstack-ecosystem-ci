58e04d0c65458732036146c35b19684161572daa
neverland <chenjiahan.jait@bytedance.com>
2026-01-06 15:12:46 +0800

    fix

--- tests/rspack/rsbuild.ts
+++ tests/rspack/rsbuild.ts
@@ -1,5 +1,5 @@
 import type { RunOptions } from '../../types';
-import { $, cd, runInRepo } from '../../utils';
+import { runInRepo } from '../../utils';
 
 export async function test(options: RunOptions) {
   await runInRepo({
