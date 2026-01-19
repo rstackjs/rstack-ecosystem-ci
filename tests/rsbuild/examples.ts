ee5fb67dcbddb2cea428ed7d3803c11d37fda418
neverland <chenjiahan.jait@bytedance.com>
2026-01-19 13:35:52 +0800

    fix

--- tests/rsbuild/examples.ts
+++ tests/rsbuild/examples.ts
@@ -4,7 +4,7 @@ import { runInRepo } from '../../utils';
 export async function test(options: RunOptions) {
   await runInRepo({
     ...options,
-    repo: 'rstackjs/rspack-examples',
+    repo: 'rstackjs/rstack-examples',
     branch: 'main',
     test: ['build:rsbuild'],
   });
