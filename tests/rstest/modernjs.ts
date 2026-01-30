e378a30dda52aaf330cd77b9e3a3710c6810b8dd
9aoy <9aoyuao@gmail.com>
2026-01-30 11:43:46 +0800

    fix: rstest modern.js test script

--- tests/rstest/modernjs.ts
+++ tests/rstest/modernjs.ts
@@ -42,6 +42,6 @@ export async function test(options: RunOptions) {
         await cache.saveCache([nxCachePath], nxCacheKey);
       }
     },
-    test: ['test:rstest'],
+    test: ['test:ut'],
   });
 }
